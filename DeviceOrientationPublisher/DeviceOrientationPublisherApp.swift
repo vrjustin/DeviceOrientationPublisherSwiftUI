acfb77f5662461fe6f24e76fbc718609edd283e2
Justin Maronde <myemailisjustin@me.com>
2024-09-06 09:46:38 -0400

    Initial Commit

---
+++ DeviceOrientationPublisher/DeviceOrientationPublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceOrientationPublisherApp.swift
+//  DeviceOrientationPublisher
+//
+//  Created by Justin Maronde on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DeviceOrientationPublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
