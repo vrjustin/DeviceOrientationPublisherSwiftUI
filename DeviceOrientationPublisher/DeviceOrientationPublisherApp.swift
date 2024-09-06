34893b414c10fe30e90d9b9976cb7a588c10b329
Justin Maronde <myemailisjustin@me.com>
2024-09-06 09:58:44 -0400

    on init of app store publisher events in cancellables array.

--- DeviceOrientationPublisher/DeviceOrientationPublisherApp.swift
+++ DeviceOrientationPublisher/DeviceOrientationPublisherApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import Combine
 
 @main
 struct DeviceOrientationPublisherApp: App {
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init() {
+        NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification).sink { notification in
+            let currentOrientation = UIDevice.current.orientation
+            print(currentOrientation)
+        }.store(in: &cancellables)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
